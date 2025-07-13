94769561c8e29c9f3672ef1e3eaabcde298a7d0f
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2025-07-14 00:02:50 +0300

    update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
 export const ARTICLE_PER_PAGE = 6;
-const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
+const PRODUCTION_DOMAIN = "https://nextprojext.vercel.app/";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
-export const DOMAIN = DEVELOPMENT_DOMAIN;
+export const DOMAIN = process.env.NODE_ENV === 'production' 
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
